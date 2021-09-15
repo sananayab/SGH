fefb2d68fbd0ede1eebbfe7a4e4e4588f0377631
sana <53163447+sananayab@users.noreply.github.com>
2021-09-16 01:24:04 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import Home from './pages/home';
+import Communities from './pages/communities';
+import Prising from './pages/pricing';
+
+
+import  './app.css'
+
+import React from 'react'; 
+
+
+const App = () => {
+  return  <Home /> ; <Communities/> ;
+     <Pricing  />;
+  
+  
+}
+
+export default App;
---
+++ app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+
+const App =() => {
+  return(
+    <div>
+      <h1>
+
+      </h1>
+    </div>
+  )
+
+}
+
+export default App;
+
+
